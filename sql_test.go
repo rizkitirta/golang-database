c3dc83e9c4733255b036f1c8abf496b1f98f2287
rizkitirta <rizkibuwono0@gmail.com>
2022-06-01 15:22:32 +0700

    sql query with QueryContext

--- sql_test.go
+++ sql_test.go
@@ -20,3 +20,30 @@ func TestExecSql(t *testing.T) {
 
 	fmt.Println("Data berhasil disimpan")
 }
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	query := "SELECT * FROM customer"
+	rows,err := db.QueryContext(ctx,query)
+	
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int 
+		var name string
+		err = rows.Scan(&id,&name)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(id,name)
+	}
+
+	fmt.Println("Done")
+}
